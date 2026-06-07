68fa8eb83b3dd733621edeeace527174a734eaca
Harshit Kumar Prajapat <harshit.kumar3805@gmail.com>
2026-06-07 17:48:54 +0530

    New project

---
+++ Dice_rolling.py
@@ -0,0 +1,19 @@
+import random
+print("----Welcome to the dice rolling game----")
+
+print("Do you want to roll the dice?(y/n)")
+a = input("Choose: ")
+
+if(a!='y' and a!='n'):
+    print("Invalid Choice!")
+
+else:
+
+    if (a == 'y'):
+        while (a == 'y'):
+            print(f'{random.randint(1,6),random.randint(1,6)}')
+            print("Do you want to play again!")
+            a = input("")
+            
+
+    print("Thanks for Playing!")            
